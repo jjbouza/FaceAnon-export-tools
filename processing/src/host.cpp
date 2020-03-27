09175c7b2f6b15eea0796ea51c2af3c9137eb54b
Jose Bouza <josejbouza@gmail.com>
2020-03-27 16:52:31 -0400

    Finished C++ pre/post processing code.

--- processing/src/host.cpp
+++ processing/src/host.cpp
@@ -17,14 +17,15 @@ int main(){
     std::cout << "Preprocessed size:" << img_preprocessed.sizes() << std::endl;
 
     // test postprocessing
-    std::vector<torch::Tensor> olist = {torch::ones({1,2,112,112}), torch::ones({1,4,112,112}), 
-                                        torch::ones({1,2,56,56}), torch::ones({1,4,56,56}),
-                                        torch::ones({1,2,56,56}), torch::ones({1,4,56,56}),
-                                        torch::ones({1,2,56,56}), torch::ones({1,4,56,56}),
-                                        torch::ones({1,2,9,9}), torch::ones({1,4,9,9}),
-                                        torch::ones({1,2,5,5}), torch::ones({1,4,5,5})};
+    std::vector<torch::Tensor> olist = {torch::randn({1,2,112,112}), torch::randn({1,4,112,112}), 
+                                        torch::randn({1,2,56,56}), torch::randn({1,4,56,56}),
+                                        torch::randn({1,2,56,56}), torch::randn({1,4,56,56}),
+                                        torch::randn({1,2,56,56}), torch::randn({1,4,56,56}),
+                                        torch::randn({1,2,9,9}), torch::randn({1,4,9,9}),
+                                        torch::randn({1,2,5,5}), torch::randn({1,4,5,5})};
 
     torch::Tensor output = postprocess(olist);
+    std::cout << output.sizes() << std::endl;
 
     std::cout << "Done!" << std::endl;
 
--- processing/src/processing.cpp
+++ processing/src/processing.cpp
@@ -1,5 +1,6 @@
 #include "processing.h"
 
+#include <vector>
 #include <math.h>
 
 torch::Tensor preprocess(torch::Tensor img){
@@ -16,8 +17,20 @@ torch::Tensor preprocess(torch::Tensor img){
     return img;
 }
 
+
+torch::Tensor decode(torch::Tensor loc, torch::Tensor priors, std::vector<float> variances){
+    torch::Tensor e1 = priors.narrow(1, 0, 2) + loc.narrow(1, 0, 2) * priors.narrow(1, 0, 2) * variances[0];
+    torch::Tensor e2 = priors.narrow(1, 2, 2) * torch::exp(loc.narrow(1, 2, 2)*variances[1]);
+
+    torch::Tensor boxes = torch::cat({e1, e2}, 1);
+    boxes.narrow(1, 0, 2) -= boxes.narrow(1, 2, 2) / 2;
+    boxes.narrow(1, 2, 2) += boxes.narrow(1, 0, 2);
+    
+    return boxes.view({4});
+}
+
 torch::Tensor postprocess(std::vector<torch::Tensor> olist){
-    std::vector<float> bboxlist;
+    std::vector<std::vector<float>> bboxlist;
 
     //apply softmax to even elements of olist...
     for(int i = 0; i < olist.size()/2; i++){
@@ -30,8 +43,44 @@ torch::Tensor postprocess(std::vector<torch::Tensor> olist){
 
         int stride = pow(2, i+2);
         int anchor = stride*4;
-        //to do...
+        
+
+        std::vector<torch::Tensor> poss = torch::where(torch::narrow(ocls, 1, 0, 1).squeeze(1) > 0.05);
+
+        for(int i = 0; i < poss[0].sizes()[0]; i++){
+            long Iindex = poss[0][i].item<long>();
+            long hindex = poss[1][i].item<long>();
+            long windex = poss[2][i].item<long>();
+
+            float axc = stride / 2 + windex*stride;
+            float ayc = stride / 2 + hindex*stride;
+
+            float score = ocls[0][1][hindex][windex].item<long>();
+
+            torch::Tensor loc = oreg.narrow(0, 0, 1).narrow(2, hindex, 1).narrow(3, windex, 1).contiguous().view({1,4});
+
+            torch::Tensor priors = torch::ones({1, 4});
+            priors[0][0] = axc; priors[0][1] = ayc; priors[0][2] = stride * 4; priors[0][3] = stride * 4;
+
+            std::vector<float> variances = {0.1, 0.2};
+            torch::Tensor box = decode(loc, priors, variances);
+            bboxlist.push_back({box[0].item<float>(), box[1].item<float>(), box[2].item<float>(), box[3].item<float>(), score});
+        }
     }
 
-    return olist[0];
+    if(bboxlist.size() == 0)
+        return torch::zeros({1,5});
+
+    // create torch tensor
+    torch::Tensor result = torch::zeros({(long int)bboxlist.size(), (long int)bboxlist[0].size()});
+    
+    for(int i = 0; i < bboxlist.size(); i++)
+        for(int j = 0; j < bboxlist[0].size(); j++)
+            result[i][j] = bboxlist[i][j];
+
+    return result;
 }
+
+
+
+
